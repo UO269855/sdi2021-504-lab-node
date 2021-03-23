cbf773dbd066fbc29de8c169185c0f618ec1c979
Álex Caso Díaz <aleexcaso@gmai.com>
2021-03-23 10:39:35 +0100

    SDI-2021-504-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 app.set('port', 8081);
 
 require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
