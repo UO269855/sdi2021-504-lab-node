04126ea17018b0bc8d65ba89815cd6d9bf1ec305
Álex Caso Díaz <aleexcaso@gmai.com>
2021-03-23 09:37:00 +0100

    SDI-2021-504-7.1-CreacióndelproyectoTiendaVirtual

--- hello-world-server.js
+++ hello-world-server.js
@@ -3,4 +3,4 @@ http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
 }).listen(2100, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:2100/');
+console.log('Server running at http://127.0.0.1:2100/')
