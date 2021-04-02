022c43ead8ac44d77cae69b01000cdda04d07901
Álex Caso Díaz <aleexcaso@gmai.com>
2021-04-02 12:59:49 +0200

    SDI-2021-504-8.4-Registrodeusuario,autenticaciónyusodesesión.

--- app.js
+++ app.js
@@ -7,19 +7,26 @@ let bodyParser = require('body-parser');
 let mongo = require('mongodb');
 let fileUpload = require('express-fileupload');
 let gestorBD = require("./modules/gestorBD.js");
-
+let crypto = require('crypto');
+let expressSession = require('express-session');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(fileUpload());
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
 
 
 gestorBD.init(app,mongo);
 
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.slqan.mongodb.net:27017,tiendamusica-shard-00-01.slqan.mongodb.net:27017,tiendamusica-shard-00-02.slqan.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-evponj-shard-0&authSource=admin&retryWrites=true&w=majority');
-
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,17 +1,26 @@
 module.exports = function(app, swig, gestorBD) {
 
     app.get('/canciones/agregar', function (req, res) {
-        let respuesta = swig.renderFile('views/bagregar.html', {
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
 
+        let respuesta = swig.renderFile('views/bagregar.html', {
         });
         res.send(respuesta);
     })
 
     app.post("/cancion", function(req, res) {
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
         let cancion = {
             nombre : req.body.nombre,
             genero : req.body.genero,
-            precio : req.body.precio
+            precio : req.body.precio,
+            autor: req.session.usuario
         }
 
         gestorBD.insertarCancion(cancion, function(id){
