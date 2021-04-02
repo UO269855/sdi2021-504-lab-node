837a7505073687fe404a1b5f02013c3d87ebf844
Álex Caso Díaz <aleexcaso@gmai.com>
2021-04-02 11:57:26 +0200

    SDI-2021-504-8.2-Arquitecturaparaelaccesoadatos.

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ let mongo = require('mongodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.slqan.mongodb.net:27017,tiendamusica-shard-00-01.slqan.mongodb.net:27017,tiendamusica-shard-00-02.slqan.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-evponj-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 
 app.listen(app.get('port'), function() {
