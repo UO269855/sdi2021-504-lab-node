e24d42d19a7bdc23e368657d9f07b634322f0c6d
Álex Caso Díaz <aleexcaso@gmai.com>
2021-04-02 11:51:43 +0200

    SDI-2021-504-8.1-AccesoadatosdeMongodesdeNode.js

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ let app = express();
 
 let swig = require('swig');
 let bodyParser = require('body-parser');
+let mongo = require('mongodb');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.set('port', 8081);
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.slqan.mongodb.net:27017,tiendamusica-shard-00-01.slqan.mongodb.net:27017,tiendamusica-shard-00-02.slqan.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-evponj-shard-0&authSource=admin&retryWrites=true&w=majority');
+
 
 require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 
 app.listen(app.get('port'), function() {
