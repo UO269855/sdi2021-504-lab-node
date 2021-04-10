919cbb3371b8d5f29b67807f3bb5d667e87474c5
Álex Caso Díaz <aleexcaso@gmai.com>
2021-04-10 12:08:49 +0200

    SDI-2021-504-9.1-Gestión-de-colecciones,-eliminar-documento.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -11,6 +11,18 @@ module.exports = function(app, swig, gestorBD) {
         res.send(respuesta);
     })
 
+    app.get('/cancion/eliminar/:id', function (req, res) {
+        let criterio = {"_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.eliminarCancion(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+                res.redirect("/publicaciones");
+            }
+        });
+    })
+
+
     app.get('/cancion/modificar/:id', function (req, res) {
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
         gestorBD.obtenerCanciones(criterio,function(canciones){
