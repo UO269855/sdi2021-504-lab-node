00317a4875e318f861b349d8a4ca959826768898
Álex Caso Díaz <aleexcaso@gmai.com>
2021-04-19 12:11:45 +0200

    SDI-2021-504-10.5-SW-Consumir-SW-desde-NodeJS.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -239,21 +239,34 @@ module.exports = function(app, swig, gestorBD) {
         });
     });
 
-                    app.get('/cancion/:id', function (req, res) {
+    app.get('/cancion/:id', function (req, res) {
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)};
         let criterio2 = { "cancion_id" : gestorBD.mongo.ObjectID(req.params.id)};
         gestorBD.obtenerCanciones(criterio,function(canciones){
             if ( canciones == null ){
                 res.send("Error al recuperar la canción.");
             } else {
-                gestorBD.obtenerComentarios(criterio2,function(comentarios){
+                let configuracion = {
+                    url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                    method: "get",
+                    headers: {
+                        "token": "ejemplo",
+                    }
+                }
+                let rest = app.get("rest");
+                rest(configuracion, function (error, response, body) {
+                    console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                    let objetoRespuesta = JSON.parse(body);
+                    let cambioUSD = objetoRespuesta.rates.EURUSD.rate;
+                    // nuevo campo "usd"
+                    canciones[0].usd = cambioUSD * canciones[0].precio;
                     let respuesta = swig.renderFile('views/bcancion.html',
                         {
-                            cancion : canciones[0],
-                            comentarios: comentarios
+                            cancion: canciones[0]
                         });
                     res.send(respuesta);
-                });
+                })
+
 
             }
         });
